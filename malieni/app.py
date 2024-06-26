3e37c1232bd39a9d4455e3ad026b7eef616c9bf8
Malieni <gui.souza.malieni@gmail.com>
2024-06-26 20:33:38 -0300

    update de validacao de cpf e cnpj

--- malieni/app.py
+++ malieni/app.py
@@ -1,14 +1,15 @@
-from flask import Flask , render_template, request, redirect, url_for
-from validate_docbr import CPF , CNPJ
-lista_produtos = [
-        {"nome": "Coca-cola" , "descricao":"Beba água" , "preco": "5.00" , "imagem": "https://pbs.twimg.com/media/E38B0zOWQAMC1bu.jpg"},
-        {"nome": "Pepsi" , "descricao":"Ruim" , "preco": "4.00", "imagem": "https://i.ytimg.com/vi/ey24G0EzJYI/maxresdefault.jpg"},
-        {"nome": "Baly" , "descricao":"Custo beneficios" , "preco": "12.00" , "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq_wNULSboJKGGIQDg82K1h8bbtbdP_L_a8Q&s.jpg"},
-    ]
-
+import csv
+from flask import Flask , render_template , request, redirect, url_for
+from validate_docbr import CPF, CNPJ
 app = Flask(__name__)
-
-@app.route("/")
+def load_products_from_csv():
+    lista_produtos = []
+    with open('produtos.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            lista_produtos.append(row)
+    return lista_produtos
+@app.route("/home")
 def home(): 
     return '<h1>Home</h1>'
 
@@ -16,25 +17,20 @@ def home():
 def contato(): 
     return '<h1>Contato</h1>'
 
+
+def save_products_to_csv(lista_produtos):
+    with open('produtos.csv', 'w', newline='') as csvfile:
+        fieldnames = ['nome', 'descricao', 'preco', 'imagem']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for produto in lista_produtos:
+            writer.writerow(produto)
+
+lista_produtos = load_products_from_csv()
+
 @app.route("/produtos")
 def produtos():
-    return render_template("produtos.html", produtos = lista_produtos)
- 
-@app.route("/produtos/<nome>")
-def produto(nome):
-    for produto in lista_produtos:
-        if produto["nome"] == nome:
-            return render_template("produto.html", produto=produto)
-
-
-    return "Produto não existe!" @app.route('/gerar_cnpj')
-def gerar_cnpj():
-    cnpj = CNPJ()
-    novo_cnpj = cnpj.generate(True)
-    return render_template('gerar_cnpj.html', cnpj=novo_cnpj)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("produtos.html", produtos=lista_produtos)
 
 @app.route("/produtos/cadastro")
 def cadastro_produto():
@@ -48,52 +44,48 @@ def salvar_produto():
     imagem = request.form["imagem"]
     produto = {"nome": nome, "descricao": descricao, "preco": preco, "imagem": imagem}
     lista_produtos.append(produto)
-
+    save_products_to_csv(lista_produtos)
     return redirect(url_for("produtos"))
 
-app.run(port=5001)
 
-app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/gerar_cpf')
+@ app.route('/gerar_cpf', methods=['GET'])
 def gerar_cpf():
     cpf = CPF()
-    novo_cpf = cpf.generate(True)
-    return render_template('gerar_cpf.html', cpf=novo_cpf)
+    cpf_gerado = cpf.generate()
+    return render_template('gerar_cpf.html', cpf_gerado=cpf_gerado)
 
-@app.route('/gerar_cnpj')
+@ app.route('/gerar_cnpj', methods=['GET'])
 def gerar_cnpj():
     cnpj = CNPJ()
-    novo_cnpj = cnpj.generate(True)
-    return render_template('gerar_cnpj.html', cnpj=novo_cnpj)
+    cnpj_gerado = cnpj.generate()
+    return render_template('gerar_cnpj.html', cnpj_gerado=cnpj_gerado)
 
-@app.route('/validar_cpf', methods=["GET", "POST"])
+@ app.route('/validar_cpf', methods=['GET', 'POST'])
 def validar_cpf():
-    if request.method == "POST":
-        cpf_usuario = request.form["cpf_usuario"]
+    if request.method == 'POST':
         cpf = CPF()
+        cpf_usuario = request.form['cpf_usuario']
         if cpf.validate(cpf_usuario):
-            mensagem = "CPF válido!"
+            return render_template('validar_cpf.html', cpf_usuario=cpf_usuario, mensagem='CPF válido')
         else:
-            mensagem = "CPF inválido!"
-        return render_template("validar_cpf.html", mensagem=mensagem)
-    return render_template("validar_cpf.html")
+            return render_template('validar_cpf.html', cpf_usuario=cpf_usuario, mensagem='CPF inválido')
+    return render_template('validar_cpf.html')
 
-@app.route('/validar_cnpj', methods=["GET", "POST"])
+@ app.route('/validar_cnpj', methods=['GET', 'POST'])
 def validar_cnpj():
-    if request.method == "POST":
-        cnpj_usuario = request.form["cnpj_usuario"]
+    if request.method == 'POST':
         cnpj = CNPJ()
+        cnpj_usuario = request.form['cnpj_usuario']
         if cnpj.validate(cnpj_usuario):
-            mensagem = "CNPJ válido!"
+            return render_template('validar_cnpj.html', cnpj_usuario=cnpj_usuario, mensagem='CNPJ válido')
         else:
-            mensagem = "CNPJ inválido!"
-        return render_template("validar_cnpj.html", mensagem=mensagem)
-    return render_template("validar_cnpj.html")
+            return render_template('validar_cnpj.html', cnpj_usuario=cnpj_usuario, mensagem='CNPJ inválido')
+    return render_template('validar_cnpj.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+app.run(port=5001)
